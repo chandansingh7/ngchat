c025e63eb9fa70945d6aa528db5642eea95a9dd7
chandansingh7 <chandan.singh558@gmail.com>
2022-11-11 10:33:52 -0600

    Update register.service.ts

--- src/app/service/register.service.ts
+++ src/app/service/register.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 import { ChatModel } from '../models/models';
 
 @Injectable({
@@ -8,18 +8,27 @@ import { ChatModel } from '../models/models';
 })
 export class RegisterService implements OnInit {
   chatModels: ChatModel[] = []
+
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.httpClient.get<ChatModel[]>("https://us-central1-pka-forms-fef14.cloudfunctions.net/getMessages?room=Mystic-1-4-U").subscribe(
-      // map((data) => {
-      //   this.chatModels = data,
-      // })
-    )
+    //this.httpClient.get<ChatModel[]>("https://us-central1-pka-forms-fef14.cloudfunctions.net/getMessages?room=Mystic-1-4-U").subscribe()
   }
+
+
+  // ----------------
+
   getData() {
-    console.log("from getData");
-    return this.httpClient.get<ChatModel[]>("https://us-central1-pka-forms-fef14.cloudfunctions.net/getMessages?room=Mystic-1-4-U")
-      .subscribe(res => { this.chatModels = res });
+    return this.httpClient.get<ChatModel[]>("https://us-central1-pka-forms-fef14.cloudfunctions.net/getMessages?room=Mystic-1-4-U").
+      pipe(
+        map((data: ChatModel[]) => {
+          return data;
+        }), catchError(error => {
+          return throwError('Something went wrong!');
+        })
+      )
   }
 }
+
+
+  // -------------
