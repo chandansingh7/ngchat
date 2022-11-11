1dbad96e974675e955fc770a0e58782941075b1d
chandansingh7 <chandan.singh558@gmail.com>
2022-11-11 15:42:54 -0600

    view update

--- src/app/chat/chat-view/chat-view.component.ts
+++ src/app/chat/chat-view/chat-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RegisterService } from 'src/app/service/register.service';
+import { RegisterViewComponent } from '../register-view/register-view.component';
 
 @Component({
   selector: 'app-chat-view',
@@ -19,11 +20,16 @@ export class ChatViewComponent implements OnInit {
     // console.log(this.chatData[0].chatRoom);
   }
 
-  addMessage(screenName: string, message: string, chatRoom: string) {
-    const input = document.getElementById('chatTextId')?.nodeValue;
-    console.log(input);
-    const timestamp = new Date();
-    this.tableData.push({ message, timestamp, chatRoom, screenName });
+  addMessage(message: string) {
+    // const input = document.getElementById('chatTextId')?.nodeValue;
+    // console.log(input);
+    // const timestamp = new Date();
+    // const sn = this.r.userScreenName;//ok nothing on chat page its empty //un
+    // const cr = this.r.userChatRoom;
+    // this.tableData.push({ message, timestamp, cr, sn });  // even backend server data?
   }
 }
+//ERROR Error: Uncaught (in promise): NullInjectorError: R3InjectorError(AppModule)[RegisterViewComponent -> RegisterViewComponent -> RegisterViewComponent]:
 
+//ya it is working like prev
+//Nishant: I am reverting for now. See if it compiles and old functionality is there. Great
